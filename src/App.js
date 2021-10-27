7e84a52df4192408f71e6a495e91aa6b87097726
jojitoon <jojitoon@gmail.com>
2021-10-27 09:01:28 +0100

    setup basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HomePage from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='*' component={() => <h1>Not Found</h1>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
